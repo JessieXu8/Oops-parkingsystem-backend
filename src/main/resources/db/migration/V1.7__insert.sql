297483eb028e6f4af0c71186b8e8bc8246a38997
caohedan <m18390990364@163.com>
2018-07-31 21:01:17 +0800

    solve conflict

--- src/main/resources/db/migration/V1.7__insert.sql
+++ src/main/resources/db/migration/V1.7__insert.sql
@@ -1,3 +1,3 @@
 -- INSERT INTO `role` VALUES ('1', 'admin');
--- INSERT INTO `role` VALUES ('2', 'manager');
+-- INSERT INTO `role` VAgit lLUES ('2', 'manager');
 -- INSERT INTO `role` VALUES ('3', 'parkingboy');

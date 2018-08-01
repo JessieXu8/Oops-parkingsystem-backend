76cb0f9091279041f506d0c7a0c5cf9abae5bd56
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-08-01 23:17:32 +0800

    cross origin login

--- src/main/java/com/oocl/parking/controllers/LoginController.java
+++ src/main/java/com/oocl/parking/controllers/LoginController.java
@@ -11,16 +11,15 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Objects;
 
+
+@CrossOrigin
 @RestController
 @RequestMapping("")
 public class LoginController {

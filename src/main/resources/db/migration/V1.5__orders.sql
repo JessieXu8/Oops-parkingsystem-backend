dd0a6f582023c9c0aa3e742ce7381cb763e6a224
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-08-04 22:36:19 +0800

    [jessie] delete the initial orders in database

--- src/main/resources/db/migration/V1.5__orders.sql
+++ src/main/resources/db/migration/V1.5__orders.sql
@@ -8,8 +8,8 @@ boy_id BIGINT default null ,
 parkinglot_id BIGINT default null,
 created_Time varchar default null
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-INSERT INTO `orders` VALUES (1, '粤A12345', '存车', '无人处理', '指派',null ,null,null );
-INSERT INTO `orders` VALUES (2, '粤A88888', '存车', '无人处理', '指派',null ,null,null  );
+-- INSERT INTO `orders` VALUES (1, '粤A12345', '存车', '无人处理', '指派',null ,null,null );
+-- INSERT INTO `orders` VALUES (2, '粤A88888', '存车', '无人处理', '指派',null ,null,null  );
 -- INSERT INTO `orders` VALUES (2, 'car_2', '取车', '', '');
 -- INSERT INTO `orders` VALUES (3, 'car_3', '存车', '无人处理', '指派');
 -- INSERT INTO `orders` VALUES (4, 'car_4', '取车', '', '');

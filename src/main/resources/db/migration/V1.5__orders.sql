f5794336aaea86d54f87df97e0d17264652fcbc1
JESSIE XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUJE8@OOCL.COM>
2018-08-04 17:16:47 +0800

    [jessie] modify the order initial data

--- src/main/resources/db/migration/V1.5__orders.sql
+++ src/main/resources/db/migration/V1.5__orders.sql
@@ -10,10 +10,6 @@ created_Time varchar default null
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 INSERT INTO `orders` VALUES (1, '粤A12345', '存车', '无人处理', '指派',null ,null,null );
 INSERT INTO `orders` VALUES (2, '粤A88888', '存车', '无人处理', '指派',null ,null,null  );
-INSERT INTO `orders` VALUES (3, '粤C88653', '存车', '停取中', '',3 ,null,null );
-INSERT INTO `orders` VALUES (4, '粤C52333', '存车', '停取中', '',3 ,2 ,null );
-INSERT INTO `orders` VALUES (5, '粤D68933', '存车', '停取中', '',3 ,2 ,null );
-INSERT INTO `orders` VALUES (6, '粤D66733', '存车', '停取中', '',3 ,2 ,null );
 -- INSERT INTO `orders` VALUES (2, 'car_2', '取车', '', '');
 -- INSERT INTO `orders` VALUES (3, 'car_3', '存车', '无人处理', '指派');
 -- INSERT INTO `orders` VALUES (4, 'car_4', '取车', '', '');
